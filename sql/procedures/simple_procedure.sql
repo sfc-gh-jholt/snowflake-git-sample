f8f59f8c9a879a721401d3f9a68794eee6069f9c
Jason Holt <jason.holt@snowflake.com>
2024-06-14 15:19:15 -0400

    fixed procedure

--- sql/procedures/simple_procedure.sql
+++ sql/procedures/simple_procedure.sql
@@ -1,14 +1,12 @@
-CREATE OR REPLACE PROCEDURE insert_into_simple_table(value STRING)
-RETURNS STRING
-LANGUAGE SQL
+CREATE OR REPLACE PROCEDURE simple_procedure()
+  RETURNS STRING
+  LANGUAGE SQL
 AS
 $$
 BEGIN
-    -- Insert the provided value and current timestamp into the table
-    INSERT INTO simple_table (VALUE, created_at)
-    VALUES (value, CURRENT_TIMESTAMP);
-
-    -- Return a success message
-    RETURN 'Insert successful';
+  INSERT INTO simple_table (value, created_at) 
+  VALUES ('New Value', CURRENT_TIMESTAMP());
+  
+  RETURN 'Timestamp inserted successfully';
 END;
 $$;
