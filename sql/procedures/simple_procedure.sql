de028ea099df745d24749fba821c376cc4d3bda9
Jason Holt <jason.holt@snowflake.com>
2024-06-14 10:26:49 -0400

    fixed bind

--- sql/procedures/simple_procedure.sql
+++ sql/procedures/simple_procedure.sql
@@ -4,7 +4,7 @@ LANGUAGE SQL
 AS
 $$
 BEGIN
-    INSERT INTO simple_table (value, created_at) VALUES (:1, CURRENT_TIMESTAMP());
+    INSERT INTO simple_table (value, created_at) VALUES (1, CURRENT_TIMESTAMP());
     RETURN 'Insert successful';
 END;
 $$
