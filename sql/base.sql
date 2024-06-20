0e9d5f17f21c843823be50712c9471d8b9159015
Jason Holt <jason.holt@snowflake.com>
2024-06-20 16:35:04 -0400

    Update base.sql

--- sql/base.sql
+++ sql/base.sql
@@ -1,3 +1,4 @@
 EXECUTE IMMEDIATE FROM './tables/simple_table.sql';
 EXECUTE IMMEDIATE FROM './procedures/simple_procedure.sql';
 EXECUTE IMMEDIATE FROM './tasks/simple_task.sql';
+---this is a comment
